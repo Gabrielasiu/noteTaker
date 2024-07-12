7ad87de8784f95c3ccf4843e36c361a79bcd4c8b
gabriela <gabysiiu@hotmail.com>
2024-07-12 16:52:53 -0700

    notes route, index route added

--- server.js
+++ server.js
@@ -1,68 +1,72 @@
-// Import Express.js
+// // Import Express.js
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
+const fs = require('fs');
+const { clearScreenDown } = require('readline');
 // const api = require('./routes/index.js');
-
+// const notesRouter = require('./notes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// // Import custom middleware, "cLog"
-// app.use(clog);
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // Middleware for parsing JSON and urlencoded form data
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-
+// app.use('api/notes', notesRouter);
 
 //what the user will see
 app.use(express.static('public'));
 
+
 // GET Route for NOTES HTML
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => { 
+
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+  
+});
 
-// GET Route INDEX HTML
-app.get('*', (req, res) =>
+
+
+// //GET API NOTES DBJSON
+// //NOSE SI EL PARAMETRO NOTES DATA ES CORERCTO
+
+app.get('/api/notes', (req, res) => {
+    
+  fs.readFile('./db/db.json', (err, results) => {
+     if (err) {
+      throw err
+     }
+let listedNotes = JSON.parse(results);
+console.log("notes", listedNotes);
+res.json(listedNotes);
+
+  })
+});
+
+//POST 
+
+app.post('/api/notes', (req, res) => {
+
+  console.log("this is the new note", req.body);
+ 
+
+});
+
+// // GET Route INDEX HTML
+
+app.get('*', (req, res) => { // el * siempre se pone abajo 
+
   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+ });
 
 
-//GET API NOTES DBJSON
-//NOSE SI EL PARAMETRO NOTES DATA ES CORERCTO
 
-app.get('/notes', (req, res) => {
-    // Let the client know that their request was received
-    res.json(`${req.method} request received`);
-  
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-  
-    // Log our request to the terminal
-    console.info(`${req.method} request received`);
-  });
+// const notesRouter = require('./routes/notes')
 
-// POST API NOTES DBJSON
-app.post('/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-  
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-  
-    // Log our request to the terminal
-    console.info(`${req.method} request received`);
-  });
-
-
-const notesRouter = require('./routes/notes')
-
-
-app.use('/notes', notesRouter);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+
